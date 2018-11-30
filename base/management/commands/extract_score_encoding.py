e726cddc308a02d73266a697470c3bd4a78a1e8a
Alessandro Brugnetti <alessandro.brugnetti@uclouvain.be>
2018-11-30 11:34:12 +0100

    Extract XLS data

--- base/management/commands/extract_score_encoding.py
+++ base/management/commands/extract_score_encoding.py
@@ -23,15 +23,25 @@
 #    see http://www.gnu.org/licenses/.
 #
 ##############################################################################
+from operator import itemgetter
+
 from django.core.management import BaseCommand
+from django.db.models import Prefetch
+from django.utils import translation
 from openpyxl import Workbook
 
+from attribution.models.attribution import Attribution
+from base.models.entity_container_year import EntityContainerYear
+from base.models.entity_version import get_last_version
+from base.models.enums import entity_container_year_link_type, learning_container_year_types
+from base.models.learning_unit_year import LearningUnitYear
 from base.models.program_manager import ProgramManager
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         _extract_program_managers_to_xls()
+        _extract_learning_unit_with_score_responsible()
 
 
 def _extract_program_managers_to_xls():
@@ -66,3 +76,85 @@ def _extract_program_managers_to_xls():
         worksheet.append(row)
 
     workbook.save(filename='program_managers_2018.xlsx')
+
+
+def _extract_learning_unit_with_score_responsible():
+    qs_list = LearningUnitYear.objects.filter(
+        academic_year__year=2018,
+    ).exclude(
+        learning_container_year__container_type=learning_container_year_types.EXTERNAL
+    ).prefetch_related(
+        Prefetch(
+            'learning_container_year__entitycontaineryear_set',
+            queryset=EntityContainerYear.objects.filter(type=entity_container_year_link_type.REQUIREMENT_ENTITY),
+            to_attr='requirement_entity'
+        ),
+        Prefetch(
+            'attribution_set',
+            queryset=Attribution.objects.all().select_related('tutor__person'),
+            to_attr='attributions'
+        )
+    )
+
+    with translation.override('fr_BE'):
+        xls_dict = {}
+        for learning_unit_year in qs_list:
+            requirement_entity = None
+            fac_requirement_entity = None
+            code = learning_unit_year.acronym
+            type = learning_unit_year.learning_container_year.get_container_type_display() if \
+                learning_unit_year.learning_container_year else "Classe"
+            intitule = learning_unit_year.complete_title
+            tutors = set()
+            score_responsibles = set()
+            if learning_unit_year.learning_container_year and learning_unit_year.learning_container_year.requirement_entity:
+                entity = learning_unit_year.learning_container_year.requirement_entity[0].entity
+                requirement_entity = get_last_version(entity)
+
+                if requirement_entity:
+                    fac_requirement_entity = requirement_entity.find_faculty_version(learning_unit_year.academic_year)
+
+            for attribution in learning_unit_year.attributions:
+                pattern = "{last_name} {first_name} ({function})"
+                tutor_text = pattern.format(
+                    last_name=attribution.tutor.person.last_name,
+                    first_name=attribution.tutor.person.first_name,
+                    function=attribution.get_function_display()
+                )
+                tutors.add(tutor_text)
+                if attribution.score_responsible:
+                    score_responsibles.add(tutor_text)
+
+            tutors = sorted(tutors)
+            score_responsibles = sorted(score_responsibles)
+            xls_dict[code] = (
+                fac_requirement_entity.acronym if fac_requirement_entity else '',
+                requirement_entity.acronym if requirement_entity else '',
+                code,
+                type,
+                intitule,
+                '\n'.join(tutors),
+                '\n'.join(score_responsibles),
+            )
+
+    # Resolve classe requirement faculty
+    xls_dict_resolved = {code: _resolve_class(tuple, xls_dict) for code, tuple in xls_dict.items()}
+
+    # Sort result
+    xls_data = sorted(xls_dict_resolved.values(), key=itemgetter(0, 1, 2))
+    workbook = Workbook()
+    worksheet = workbook.active
+
+    worksheet.append(["Faculté de l'entité de gestion", 'Entité de gestion', 'Sigle', 'Type', 'Intitulé',
+                      'Enseignant(s)', 'Responsable de notes'])
+    for row in xls_data:
+        worksheet.append(list(row))
+    workbook.save(filename='learning_unit_2018.xlsx')
+
+
+def _resolve_class(tuple, xls_dict):
+    if tuple[3] == 'Classe':
+        tuple_parent = xls_dict.get(tuple[2][:-1])
+        if tuple_parent:
+            return (tuple_parent[0], tuple_parent[1],) + (tuple[2], tuple[3], tuple[4], tuple[5], tuple[6], )
+    return tuple
